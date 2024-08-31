f10e8c70df8d1e2107aab30714030ab55e523aba
Ekta Mehra <mehra.ekta95@gmail.com>
2024-08-31 15:06:24 +0000

    Add js code for start-quiz area of teh quiz

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+let currentQuestionIndex = 0;
+let questions = [];
+function loadQuestion() {
+  let questionObj = questions[currentQuestionIndex];
+  let questionContainer = document.getElementById("question-container");
+}
